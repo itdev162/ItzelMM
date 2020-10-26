cfb2c85f900b037bffb33b76c144b262a437c93f
Itzel-MM <montali1@gmatc.matc.edu>
2020-10-26 12:02:47 -0500

    Issues Activity #

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,24 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope()) 
+            {
+                var Services = scope.ServiceProvider;
+                try 
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
