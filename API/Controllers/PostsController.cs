4d62f1450f763f6ff1bcd2b57a7f4ced29033904
Itzel-MM <montali1@gmatc.matc.edu>
2020-11-16 19:17:28 -0600

    Activity 6 semi-completed

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -41,5 +41,62 @@ namespace API.Controllers
         {
             return this.context.Posts.Find(id);
         }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost]
+
+        public ActionResult<Posts> Create([FromBody]PostsController request)
+        {
+            var post = new Posts
+            {
+                Id = request.GetById,
+                Title = request.Title,
+                Body = request.Body,
+                DateTime = request.Date
+            };
+            ContextBoundObject.Posts.Add(post);
+            var success = ContextBoundObject.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+            throw new Exception("Error creating post");
+        }
+
+        /// <summary>
+        /// PUT api/put
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns>An updated post</returns>
+        [HttpPut]
+
+        public ActionResult<Post> Update([FromBody]PostsController request)
+        {
+            var post = ContextBoundObject.Posts.Find(request.Id);
+
+            if (post == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            // Update the post properties with request values, if present.
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != null ? request.Date : post.Date;
+
+            var success = ContextBoundObject.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error updating post");
+        }
     }
 }
